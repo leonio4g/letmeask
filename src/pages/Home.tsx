26753f851571be6a5f431bcff6c61fdc6b21b7f9
Leonio Santos <leonio.santos@secom.local>
2021-06-23 13:24:49 -0300

    fazendo a criação de sala e entradas de salas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react'
 import { useHistory } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import illustrationImg from '../assets/images/illustration.svg';
@@ -6,17 +7,36 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import Button from '../components/Button';
 
 import '../styles/auth.scss';
+import { database } from '../services/firebase';
 
-export default function Home() {
+export function Home() {
 
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [ roomCode, setRoomCode ] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
             await signInWithGoogle();
         }
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event : FormEvent){
+        event.preventDefault();
+
+        if(roomCode.trim() === ''){
+            return;
+        }
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()){
+            alert('Room does not exists.')
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
+
     }
 
     return (
@@ -34,10 +54,12 @@ export default function Home() {
                         Crie sua sala com Google
                     </button>
                     <div className="separator" >Ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom} >
                         <input
                             type="text"
                             placeholder="Digite codigo da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">Entrar na sala</Button>
 
